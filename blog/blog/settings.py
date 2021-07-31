687981d7a2f96f63c79d77cc35d8b9d4d57e4e7a
your lyh001126 <your 3067303510@qq.com>
2021-07-31 14:49:37 +0800

    '数据库配置'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'lyh',  # 数据库的用户名
+        'NAME': 'blog',  # 数据库名
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'PASSWORD': '5201314a'
     }
 }
 
